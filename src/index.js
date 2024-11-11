d2940935c06bbaa142b52ddef9b50d36b48b628d
Caleb Raney <110679107+calebraney@users.noreply.github.com>
2024-11-11 09:29:29 -0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-// v1.1 flowbuilder Form Interaction
+// v1.2 flowbuilder Form Interaction
 document.addEventListener('DOMContentLoaded', function () {
   //////////////////////////////
   const dynamicForm = function () {
@@ -18,24 +18,35 @@ document.addEventListener('DOMContentLoaded', function () {
     const downloadButtons = [...document.querySelectorAll(`[${DOWNLOAD_BUTTON}]`)];
     if (formButtons.length === 0) return;
     let redirectUrl;
+    let currentCookie;
     //on load check for cookie and if set change the buttons visibility
-    if (localStorage.getItem(SUBMIT_COOKIE) !== null) {
-      // hide form buttons
-      formButtons.forEach((formButton, index) => {
-        formButton.style.display = 'none';
-      });
-      downloadButtons.forEach((downloadButton, index) => {
-        downloadButton.style.display = 'block';
-      });
-    }
-
-    //update form and redirect on button clicks
+    // if (localStorage.getItem(SUBMIT_COOKIE) !== null) {
+    //   // hide form buttons
+    //   formButtons.forEach((formButton, index) => {
+    //     formButton.style.display = 'none';
+    //   });
+    //   downloadButtons.forEach((downloadButton, index) => {
+    //     downloadButton.style.display = 'block';
+    //   });
+    // }
+    // for each form button
     formButtons.forEach((formButton, index) => {
+      const downloadButton = downloadButtons[index];
+      const cookie = `${SUBMIT_COOKIE}-${index}`;
+      //check if the cookie for this button exist, and if it does change the buttons visibility
+      if (localStorage.getItem(cookie) !== null) {
+        formButton.style.display = 'none';
+        downloadButton.style.display = 'block';
+      }
+
+      //update form and redirect on button clicks
       //when button is clicked updated hidden form field and redirect url
       formButton.addEventListener('click', () => {
+        //get the redirect url from the form button and update the variable and hidden form field
         redirectUrl = formButton.getAttribute(FORM_BUTTON);
-        console.log(redirectUrl);
         hiddenInput.value = redirectUrl;
+        //update the cookie variable to be equal to the current button
+        currentCookie = cookie;
       });
     });
 
@@ -44,8 +55,7 @@ document.addEventListener('DOMContentLoaded', function () {
       // when form is submitted
       if (successMessage.style.display == 'block') {
         //create cookie to show download buttons
-        localStorage.setItem(SUBMIT_COOKIE, 'true');
-
+        localStorage.setItem(currentCookie, 'true');
         //redirect user
         window.location.href = redirectUrl;
       }
