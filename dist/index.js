a5335ea62261747bea06a6b4c66d0e56eafd951c
Caleb Raney <110679107+calebraney@users.noreply.github.com>
2024-11-21 16:44:01 -0800

    fixed flow split interaction

--- dist/index.js
+++ dist/index.js
@@ -7,115 +7,132 @@
 
   // src/index.js
   document.addEventListener("DOMContentLoaded", function() {
-    const chart = function() {
-      const CHART_SELECTOR = "#chart-discount";
-      const LINE_COLOR = "#00BAE1";
-      const CHART_BORDER = "#C2D1D8";
-      const BORDER_WIDTH = 2;
-      const SHOW_GRID_LINES = false;
-      const data = [
-        { date: "09/15/2024", percent: 0 },
-        { date: "09/16/2024", percent: -0.25 },
-        { date: "09/19/2024", percent: 0 },
-        { date: "09/22/2024", percent: 0.01 },
-        { date: "09/26/2024", percent: -0.1 },
-        { date: "09/30/2024", percent: 0.1 },
-        { date: "10/01/2024", percent: 0 },
-        { date: "10/03/2024", percent: 0.01 },
-        { date: "10/04/2024", percent: 0.05 },
-        { date: "10/04/2024", percent: -0.01 },
-        { date: "10/05/2024", percent: -0.07 },
-        { date: "10/05/2024", percent: 0.05 },
-        { date: "10/07/2024", percent: 0.07 },
-        { date: "10/09/2024", percent: 0 },
-        { date: "10/11/2024", percent: 0.03 },
-        { date: "10/13/2024", percent: 0.15 },
-        { date: "10/14/2024", percent: -0.1 },
-        { date: "10/15/2024", percent: -0.03 },
-        { date: "10/15/2024", percent: 0.01 },
-        { date: "10/17/2024", percent: 0.02 },
-        { date: "11/01/2024", percent: 0.07 },
-        { date: "11/05/2024", percent: 0.1 },
-        { date: "11/01/2024", percent: 0.11 },
-        { date: "11/02/2024", percent: 0.02 },
-        { date: "11/03/2024", percent: -0.01 },
-        { date: "11/04/2024", percent: -0.12 },
-        { date: "11/10/2024", percent: 0 },
-        { date: "11/10/2024", percent: 0.04 }
-      ];
-      console.log("hi");
-      const chartElement = document.querySelector(CHART_SELECTOR);
-      if (!chartElement) return;
-      Chart.defaults.borderColor = CHART_BORDER;
-      Chart.defaults.borderWidth = BORDER_WIDTH;
-      Chart.defaults.scales.category.title = {
-        ...Chart.defaults.scales.category.title,
-        font: {
-          size: 20
-        }
-      };
-      Chart.defaults.scales.linear.title = {
-        ...Chart.defaults.scales.linear.title,
-        font: {
-          size: 20
-        }
-      };
-      const chart2 = new Chart(chartElement, {
-        type: "line",
-        options: {
-          responsive: true,
-          maintainAspectRatio: false,
-          animation: true,
-          elements: {
-            line: { borderColor: LINE_COLOR },
-            point: { borderColor: LINE_COLOR, backgroundColor: LINE_COLOR, radius: 1 }
-          },
-          scales: {
-            x: {
-              ticks: { maxTicksLimit: 9 },
-              grid: {
-                //dont show grid lines
-                drawOnChartArea: SHOW_GRID_LINES
-              }
-            },
-            y: {
-              max: 1,
-              min: -1,
-              // backgroundColor: '#000',
-              borderColor: "#000",
-              ticks: {
-                callback: (value) => `${value.toFixed(2)}%`
-              },
-              title: {
-                text: "Premium Discount (%)",
-                display: true
-              },
-              grid: {
-                //dont show grid lines
-                drawOnChartArea: SHOW_GRID_LINES
-              }
-            }
-          },
-          plugins: {
-            legend: {
-              display: false
-            },
-            tooltip: {
-              enabled: true
-            }
-          }
-        },
-        data: {
-          labels: data.map((row) => row.date),
-          datasets: [
-            {
-              label: "Premium Discount Percent",
-              data: data.map((row) => row.percent)
-            }
-          ]
+    function LottieScrollTrigger(vars) {
+      let playhead = { frame: 0 }, target = gsap.utils.toArray(vars.target)[0], speeds = { slow: "+=2000", medium: "+=1000", fast: "+=500" }, st = {
+        trigger: target,
+        pin: false,
+        start: "top 45%",
+        end: speeds[vars.speed] || "+=1000",
+        scrub: 1
+      }, ctx = gsap.context && gsap.context(), animation = lottie.loadAnimation({
+        container: target,
+        renderer: vars.renderer || "svg",
+        loop: false,
+        autoplay: false,
+        path: vars.path,
+        rendererSettings: vars.rendererSettings || {
+          preserveAspectRatio: "xMidYMid slice"
         }
       });
+      for (let p in vars) {
+        st[p] = vars[p];
+      }
+      animation.addEventListener("DOMLoaded", function() {
+        let createTween = function() {
+          animation.frameTween = gsap.to(playhead, {
+            frame: animation.totalFrames - 1,
+            ease: "none",
+            onUpdate: () => animation.goToAndStop(playhead.frame, true),
+            scrollTrigger: st
+          });
+          return () => animation.destroy && animation.destroy();
+        };
+        ctx && ctx.add ? ctx.add(createTween) : createTween();
+        ScrollTrigger.sort();
+        ScrollTrigger.refresh();
+      });
+      return animation;
+    }
+    const setupLotties = function() {
+      const SECTION = ".section";
+      const LOTTIE_WRAP = ".flow-split_image-wrap";
+      const LOTTIE_EL = ".flow-split_image";
+      const SCROLL_TRIGGER = ".flow-split_content";
+      const ACTIVE_CLASS = "is-active";
+      const sections = [...document.querySelectorAll(SECTION)];
+      sections.forEach((section, index) => {
+        const scrollTriggers = [...section.querySelectorAll(SCROLL_TRIGGER)];
+        const items = [...section.querySelectorAll(LOTTIE_WRAP)];
+        const firstItem = items[0];
+        const lastItem = items[items.length - 1];
+        items.forEach((item, index2) => {
+          const scrollTrigger = scrollTriggers[index2];
+          const lottieOld = item.querySelector(LOTTIE_EL);
+          let lottieNew, animation;
+          if (!item || !scrollTrigger) return;
+          const lottiePath = lottieOld.dataset.src;
+          const vars = {
+            trigger: scrollTrigger,
+            start: "top center",
+            end: "bottom center",
+            markers: false,
+            scrub: 1
+            // seconds it takes for the playhead to "catch up"
+          };
+          const animationType = lottieOld.dataset.animationType;
+          let lottieAnimation = true;
+          if (animationType !== "lottie") {
+            lottieAnimation = false;
+          }
+          if (lottieAnimation) {
+            lottieOld.remove();
+            lottieNew = document.createElement("div");
+            item.appendChild(lottieNew);
+            lottieNew.classList.add(LOTTIE_EL);
+            animation = LottieScrollTrigger({
+              trigger: vars.trigger,
+              target: lottieNew,
+              path: lottiePath,
+              start: vars.start,
+              end: vars.end,
+              markers: vars.markers,
+              scrub: vars.scrub
+            });
+          }
+          const activateItem = function(activate = true) {
+            if (activate) {
+              item.classList.add(ACTIVE_CLASS);
+              if (lottieAnimation) {
+                animation.play();
+                console.log("play", index2);
+              }
+            } else {
+              item.classList.remove(ACTIVE_CLASS);
+            }
+          };
+          let tl = gsap.timeline({
+            scrollTrigger: {
+              trigger: vars.trigger,
+              start: vars.start,
+              end: vars.end,
+              markers: vars.markers,
+              scrub: vars.scrub,
+              onEnter: () => {
+                activateItem();
+              },
+              onLeave: () => {
+                if (item !== lastItem) {
+                  activateItem(false);
+                }
+              },
+              onEnterBack: () => {
+                activateItem();
+              },
+              onLeaveBack: () => {
+                if (item !== firstItem) {
+                  activateItem(false);
+                }
+              }
+            }
+          });
+        });
+      });
     };
-    chart();
+    let mm = gsap.matchMedia();
+    mm.add("(min-width: 768px)", () => {
+      setupLotties();
+      return () => {
+      };
+    });
   });
 })();
